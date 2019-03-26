304e88b5884999616e24b1ffd52f9520ac22172d
Visvash <44068838+visvash@users.noreply.github.com>
2019-03-26 19:42:56 +0530

    flute

---
+++ FLUTE/Notes_for_flute_servo.ino
@@ -0,0 +1,202 @@
+void C(){
+  pressure(stepInit,stepC);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=0;
+  posInit2=30;
+  posInit3=30;
+  posInit4=30;
+  posInit5=0;
+  posInit6=30;
+}
+
+void C_High(){
+  pressure(stepInit,stepC_High);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=3;
+  posInit2=63;
+  posInit3=63;
+  posInit4=32;
+  posInit5=32;
+  posInit6=32;
+}
+
+void D(){
+  pressure(stepInit,stepD);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 0;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=0;
+  posInit2=30;
+  posInit3=0;
+  posInit4=30;
+  posInit5=0;
+  posInit6=30;
+}
+
+void D_High(){
+  pressure(stepInit,stepD_High);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 0;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=3;
+  posInit2=63;
+  posInit3=39;
+  posInit4=32;
+  posInit5=32;
+  posInit6=32;
+}
+
+void E(){
+  pressure(stepInit,stepE);
+  currFlag1 = 1;
+  currFlag2 = 0;
+  currFlag3 = 0;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=0;
+  posInit2=0;
+  posInit3=0;
+  posInit4=30;
+  posInit5=0;
+  posInit6=30;
+}
+
+void F_note(){
+  pressure(stepInit,stepF);
+  currFlag1 = 0;
+  currFlag2 = 0;
+  currFlag3 = 0;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=30;
+  posInit2=0;
+  posInit3=0;
+  posInit4=30;
+  posInit5=0;
+  posInit6=30;
+}
+
+void F_High(){
+  pressure(stepInit,stepF_High);
+  currFlag1 = 0;
+  currFlag2 = 0;
+  currFlag3 = 0;
+  currFlag4 = 0;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=32;
+  posInit2=39;
+  posInit3=39;
+  posInit4=32;
+  posInit5=32;
+  posInit6=32;}
+
+void G(){
+  pressure(stepInit,stepG);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 1;
+  currFlag5 = 1;
+  currFlag6 = 1;
+  newNote();
+  posInit1=0;
+  posInit2=30;
+  posInit3=30;
+  posInit4=0;
+  posInit5=30;
+  posInit6=0;
+}
+
+void G_High(){
+  pressure(stepInit,stepG_sharp);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 1;
+  currFlag5 = 1;
+  currFlag6 = 1;
+  newNote();
+  posInit1=3;
+  posInit2=63;
+  posInit3=63;
+  posInit4=3;
+  posInit5=3;
+  posInit6=3;}
+
+void A(){
+  pressure(stepInit,stepA);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 1;
+  currFlag5 = 1;
+  currFlag6 = 0;
+  newNote();
+  posInit1=0;
+  posInit2=30;
+  posInit3=30;
+  posInit4=0;
+  posInit5=30;
+  posInit6=30;
+}
+
+void A_High(){
+  pressure(stepInit,stepA_High);
+ 
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 1;
+  currFlag5 = 1;
+  currFlag6 = 0;
+  newNote();
+  posInit1=3;
+  posInit2=63;
+  posInit3=63;
+  posInit4=3;
+  posInit5=3;
+  posInit6=32;
+}
+
+void B(){
+  pressure(stepInit,stepB);
+  currFlag1 = 1;
+  currFlag2 = 1;
+  currFlag3 = 1;
+  currFlag4 = 1;
+  currFlag5 = 0;
+  currFlag6 = 0;
+  newNote();
+  posInit1=0;
+  posInit2=30;
+  posInit3=30;
+  posInit4=0;
+  posInit5=0;
+  posInit6=30;
+}
